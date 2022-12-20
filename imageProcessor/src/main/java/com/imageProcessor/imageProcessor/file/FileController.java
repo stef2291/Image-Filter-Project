138ad0362168b3b19b327812c70c62d7ee99ed64
Teddy Putus <99917582+TeddyPutus@users.noreply.github.com>
2022-12-20 11:18:20 +0000

    created a route to test if Java can execute the CPP exe

--- imageProcessor/src/main/java/com/imageProcessor/imageProcessor/file/FileController.java
+++ imageProcessor/src/main/java/com/imageProcessor/imageProcessor/file/FileController.java
@@ -58,6 +58,7 @@ public class FileController {
                 "You successfully uploaded " + file.getOriginalFilename() + "!");
 
 //        Grayscale.createGrayscale(filepath);
+        Process process = new ProcessBuilder("C:\\PathToExe\\MyExe.exe",filepath,"greyscale").start();
 
         Resource fileToSend = storageService.loadAsResource(Grayscale.createGrayscale(filepath).getName());
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
@@ -82,6 +83,24 @@ public class FileController {
 
     }
 
+    //make image black and white
+    @GetMapping("/file/grayscale/{userId}/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveBlackAndWhiteFile(@PathVariable Long  userId, @PathVariable String filename) throws IOException {
+
+        //confirm file exists, confirm it is associated with user
+        File fileToSend = fileService.getOneUserFileByName(userId, filename);
+
+        Resource file = storageService.loadAsResource(fileToSend.getFilename());
+
+        Process process = new ProcessBuilder("C:\\PathToExe\\MyExe.exe",fileToSend.getFilepath(),"greyscale").start();
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+
+    }
+
+
     //Delete one file
     @DeleteMapping("/file/{userId}/{filename:.+}")
     public Boolean deleteOneFile(@PathVariable Long  userId, @PathVariable String filename) throws IOException {
@@ -95,43 +114,29 @@ public class FileController {
 
     //Send the image to be turned black and white, store the image and add entry in DB
     //This is a post request we are MAKING, not receiving
-    public ResponseEntity<Resource> postBlackAndWhiteImage(String filename, Long userId) {
-        String url = "https://localhost:5001/files/testing"; //TBC with Olly
+//    public ResponseEntity<Resource> postBlackAndWhiteImage(String filename, Long userId) {
 
-        // create headers
-        HttpHeaders headers = new HttpHeaders();
-        // set `content-type` header
-//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-//        headers.setAccept(Collections.singletonList(MediaType.MULTIPART_FORM_DATA));
 
-        Resource file = storageService.loadAsResource(filename); //load the resource to send
-        //Create a key value pair in the form of a map that we can send to be processed
-        MultiValueMap<String, Resource> dataToPost = new LinkedMultiValueMap<>();
-        dataToPost.add("file", file);
+//        String url = "https://localhost:5001/files/testing"; //TBC with Olly
+//
+//        // create headers
+//        HttpHeaders headers = new HttpHeaders();
+//        // set `content-type` header
+////        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+////        headers.setAccept(Collections.singletonList(MediaType.MULTIPART_FORM_DATA));
+//
+//        Resource file = storageService.loadAsResource(filename); //load the resource to send
+//        //Create a key value pair in the form of a map that we can send to be processed
+//        MultiValueMap<String, Resource> dataToPost = new LinkedMultiValueMap<>();
+//        dataToPost.add("file", file);
+//
+//        HttpEntity<MultiValueMap<String, Resource>> entity = new HttpEntity<>(dataToPost, headers);
+//
+//        //send and wait for the response
+//        ResponseEntity<Resource> response = this.restTemplate.postForEntity(url, entity, Resource.class);
+//
+////        fileService.saveFile(response.getBody(), userId); //save the black and white file
+//        return response; //return the black and white file, so we can send it back to the user
+//    }
 
-        HttpEntity<MultiValueMap<String, Resource>> entity = new HttpEntity<>(dataToPost, headers);
-
-        //send and wait for the response
-        ResponseEntity<Resource> response = this.restTemplate.postForEntity(url, entity, Resource.class);
-
-//        fileService.saveFile(response.getBody(), userId); //save the black and white file
-        return response; //return the black and white file, so we can send it back to the user
-    }
-
-    @PostMapping("/testing") //Here we need to do the extra step of creating a new file model in database
-    public ResponseEntity<Resource> testUploadCPP(@RequestBody MultipartFile file,
-                                   RedirectAttributes redirectAttributes)
-                                    {
-
-        fileService.saveFile(file, 1L);
-//        storageService.store(file); //do this in the fileService - makes more sense
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
-        File fileToSend = fileService.getOneUserFileByName(1L, file.getOriginalFilename());
-
-        Resource fileToRespond = storageService.loadAsResource(fileToSend.getFilename());
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + fileToRespond.getFilename() + "\"").body(fileToRespond);
-    }
 }
